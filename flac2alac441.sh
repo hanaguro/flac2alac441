f4b1b7a7744352a624297b35aca30efeef98d70c
Takahiro Yoshizawa <kuro@takahiro.org>
2017-05-25 02:15:45 +0000

    スペースの含まれるファイル名を変換できなかったので修正

--- flac2alac441.sh
+++ flac2alac441.sh
@@ -1,5 +1,6 @@
-for var in `ls -1 *.flac`
+#for var in `ls -1 *.flac`
+for var in *.flac
 do
-	ffmpeg -i $var -ac 2 -ar 44100 -acodec alac ${var%%.flac}.m4a
+	ffmpeg -i "$var" -ac 2 -ar 44100 -acodec alac "${var%%.flac}.m4a"
 done
 
